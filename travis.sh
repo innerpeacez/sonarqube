7019f27a21fec4b57884076ca7db1cdb03118a4b
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2018-11-14 10:21:47 +0100

    Update Travis build

--- travis.sh
+++ travis.sh
@@ -4,33 +4,6 @@ set -euo pipefail
 ./.travis/setup_ramdisk.sh
 
 #
-# A (too) old version of JDK8 is installed by default on Travis.
-# This method is preferred over Travis apt oracle-java8-installer because
-# JDK is kept in cache. It does not need to be downloaded from Oracle
-# at each build.
-#
-function installJDK8 {
-  JDK_RELEASE=181
-  echo "Setup JDK 1.8u$JDK_RELEASE"
-  mkdir -p ~/jvm
-  pushd ~/jvm > /dev/null
-  if [ ! -d "jdk1.8.0_$JDK_RELEASE" ]; then
-    {
-      wget --quiet --continue --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz
-    } || {
-      echo "failed to download JDK 1.8u$JDK_RELEASE"
-      exit 1
-    }
-    tar xzf jdk-8u$JDK_RELEASE-linux-x64.tar.gz
-    rm jdk-8u$JDK_RELEASE-linux-x64.tar.gz
-  fi
-  popd > /dev/null
-  export JAVA_HOME=~/jvm/jdk1.8.0_$JDK_RELEASE
-  export PATH=$JAVA_HOME/bin:$PATH
-  echo "JDK 1.8u$JDK_RELEASE installed"
-}
-
-#
 # Maven 3.2.5 is installed by default on Travis. Maven 3.5 is preferred.
 #
 function installMaven {
@@ -112,7 +85,7 @@ function fixBuildVersion {
 #
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v41 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v50 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
@@ -127,7 +100,6 @@ case "$TARGET" in
 
 BUILD)
 
-  installJDK8
   installMaven
   installNode
   fixBuildVersion
